f6f73c45659432b6fed7d655afab6c22af357f94
mrzapp <contact@jeppezapp.com>
2017-09-15 15:33:23 +0200

    Updated docs

--- js/client.js
+++ js/client.js
@@ -4,7 +4,7 @@
  * Scroll behaviour
  */
 window.onscroll = function(e) {
-    const STICKY_NAV_THRESHOLD = 10;
+    var STICKY_NAV_THRESHOLD = 10;
 
     document.body.classList.toggle('fixed-nav', document.body.scrollTop > STICKY_NAV_THRESHOLD);
 };
